6e2bfe72bcd148f1785034cc65d213956855d2d1
mayankaryaca <mayankaryaca@gmail.com>
2021-06-03 12:45:15 -0400

    Home botton navigation bar + home add parking button + login UI

--- app/src/main/java/com/example/maple/LoginScreenActivity.java
+++ app/src/main/java/com/example/maple/LoginScreenActivity.java
@@ -3,12 +3,22 @@ package com.example.maple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.maple.databinding.ActivityLoginScreenBinding;
+import com.example.maple.databinding.ActivityMainBinding;
 
 public class LoginScreenActivity extends AppCompatActivity {
 
+    ActivityLoginScreenBinding binding;
+    public final String TAG = this.getClass().getCanonicalName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        this.binding = ActivityLoginScreenBinding.inflate(getLayoutInflater());
+        View view = this.binding.getRoot();
+        setContentView(view);
+
     }
+
 }
